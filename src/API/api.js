28b62b2f6d036d5b196bb76d0ad6d504e87df0dc
lafBot <64456835+lafBot@users.noreply.github.com>
2021-03-26 14:42:43 -0600

    Updated api url

--- src/API/api.js
+++ src/API/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:5000";
+const BASE_URL = "https://re-crm-backend.herokuapp.com/";
 
 async function submitOfferToAPI(data) {
     try {
